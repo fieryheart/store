74f9bd6b44b5b5ce57a8ab88779a72f040ae039d
fieryheart <969216914@qq.com>
2016-12-15 22:30:19 +0800

    ip

--- my storehouse/works/chrome-extensions/my-clock/js/my_ip.js
+++ my storehouse/works/chrome-extensions/my-clock/js/my_ip.js
@@ -9,7 +9,15 @@ function httpRequest(url, callback){
     xhr.send();
 }
 
-httpRequest('http://ip.chinaz.com/getip.aspx', function(ip){
-  var obj = JSON.parse(ip);
-  document.getElementById('ip_div').innerText = obj.ip + "<br>" + obj.address;
+httpRequest('http://ip.chinaz.com/getip.aspx', function(data,status){
+  var ip_obj = data.replace("{","").replace("}","").split(",");
+  var ip_arr = [];
+  var ip_json = {};
+  for(let i = 0; i < ip_obj.length; i++){
+    ip_arr = ip_obj[i].replace("'","").replace("'","").split(":");
+    ip_json[ip_arr[0]] = ip_arr[1];
+  }
+  console.log(ip_json);
+  document.getElementById('ip').innerText = ip_json.ip;
+  document.getElementById('address').innerText = ip_json.address;
 });
