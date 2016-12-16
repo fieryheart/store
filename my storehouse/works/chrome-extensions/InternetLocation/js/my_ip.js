48fecad9ef48015b8448c43d6f19a44ad08fe391
fieryheart <969216914@qq.com>
2016-12-16 13:46:20 +0800

    InternetLocation v1.1

--- my storehouse/works/chrome-extensions/InternetLocation/js/my_ip.js
+++ my storehouse/works/chrome-extensions/InternetLocation/js/my_ip.js
@@ -15,29 +15,35 @@ var User = function(){
   this.id = null;
   this.ip = null;
   this.address = null;
-  this.on = function(id){
+  this.on = function(id, data){
     var obj = document.getElementById(id);
-    obj.addEventListener('click' , function(){
-      console.log(this[id]);
-      this.innerText = this[id];
+    var objT = document.getElementById(id+'T');
+    var item = data;
+    var i = 0;
+    obj.addEventListener('click' , function(item){
+
+      if(i % 2){
+        objT.innerText = '';
+      }else{
+        objT.innerText = data;
+      }
+      i++;
     });
   }
 }
-
-
 
 httpRequest('http://ip.chinaz.com/getip.aspx', function(data,status){
   var obj = data.replace("{","").replace("}","").split(",");
   var info_arr = [];
   var info_json = {};
   var user = new User();
+  var i = 0;
   for(let i = 0; i < obj.length; i++){
     info_arr = obj[i].replace("'","").replace("'","").split(":");
     info_json[info_arr[0]] = info_arr[1];
   }
   user.ip = info_json.ip;
   user.address = info_json.address;
-  console.log(user);
-  user.on('ip');
-  user.on('address');
+  user.on('ip', user.ip);
+  user.on('address', user.address);
 });
