4186f4dca7a4db347ef3241a3f9477512a5d46c6
fieryheart <969216914@qq.com>
2016-12-16 12:30:44 +0800

    InternetLocation v1.0

--- my storehouse/works/chrome-extensions/InternetLocation/js/my_ip.js
+++ my storehouse/works/chrome-extensions/InternetLocation/js/my_ip.js
@@ -9,15 +9,35 @@ function httpRequest(url, callback){
     xhr.send();
 }
 
-httpRequest('http://ip.chinaz.com/getip.aspx', function(data,status){
-  var ip_obj = data.replace("{","").replace("}","").split(",");
-  var ip_arr = [];
-  var ip_json = {};
-  for(let i = 0; i < ip_obj.length; i++){
-    ip_arr = ip_obj[i].replace("'","").replace("'","").split(":");
-    ip_json[ip_arr[0]] = ip_arr[1];
+
+
+var User = function(){
+  this.id = null;
+  this.ip = null;
+  this.address = null;
+  this.on = function(id){
+    var obj = document.getElementById(id);
+    obj.addEventListener('click' , function(){
+      console.log(this[id]);
+      this.innerText = this[id];
+    });
   }
-  console.log(ip_json);
-  document.getElementById('ip').innerText = ip_json.ip;
-  document.getElementById('address').innerText = ip_json.address;
+}
+
+
+
+httpRequest('http://ip.chinaz.com/getip.aspx', function(data,status){
+  var obj = data.replace("{","").replace("}","").split(",");
+  var info_arr = [];
+  var info_json = {};
+  var user = new User();
+  for(let i = 0; i < obj.length; i++){
+    info_arr = obj[i].replace("'","").replace("'","").split(":");
+    info_json[info_arr[0]] = info_arr[1];
+  }
+  user.ip = info_json.ip;
+  user.address = info_json.address;
+  console.log(user);
+  user.on('ip');
+  user.on('address');
 });
