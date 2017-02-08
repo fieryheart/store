58a08a6ca75c0c99687e723f496c097b665b35f0
fieryheart <969216914@qq.com>
2017-02-08 23:38:46 +0800

    es6

---
+++ my storehouse/study/ES6 in es6katas.org/#50_Generator_iterator.js
@@ -0,0 +1,9 @@
+// h1、一个generator返回一个可遍历的对象
+
+// 1、一个generator返回一个对象
+
+// 2、一个 generator对象有key——`Symbol.iterator`
+
+// 3、`Symbol.iterator`是一个函数
+
+// 4、可以循环通过`for-of`
---
+++ my storehouse/study/ES6 in es6katas.org/#51_Generator_yieldExpressions.js
@@ -0,0 +1,9 @@
+// h1、generator-`yield`用来停止和恢复一个generator函数
+
+// 1、将一个generator转变成一个数组恢复generators知道所有的值都被接受
+
+// 2、在第一个`genertor.next()`之后
+
+// 3、在第二个`next()`调用之后
+
+// 4、在到达最后一个元素，调用`next()`
