ef51ca7ef76f1917b812277300637702e0e1c07a
fieryheart <969216914@qq.com>
2017-02-08 15:30:17 +0800

    es6

---
+++ my storehouse/study/ES6 in es6katas.org/#45_map_mapGet.js
@@ -0,0 +1,9 @@
+// h1、`Map.prototype.get`返回map中的给定key的元素
+
+// 1、`get(key)`返回根据这个key存储的值
+
+// 2、通过调用`set()`设定值
+
+// 3、没有这个key将返回undefined
+
+// 4、返回undefined对于没有的key
---
+++ my storehouse/study/ES6 in es6katas.org/#46_map_mapSet.js
@@ -0,0 +1,9 @@
+// h1、`Map.prototype.set`给map增加一个key/value的元素
+
+// 1、简单的情况`set(key, value)`和`get(key)`
+
+// 2、key可以是一个复杂的类型
+
+// 3、用同样的key再次调用`set()`会代替原来的值
+
+// 4、`set()`返回这个map对象，且是链式的
