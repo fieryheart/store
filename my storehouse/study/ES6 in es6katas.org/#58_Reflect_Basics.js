45570740f88c354e3f93a3e77b6af0d64ed9ff6c
fieryheart <969216914@qq.com>
2017-02-09 17:18:16 +0800

    es6

---
+++ my storehouse/study/ES6 in es6katas.org/#58_Reflect_Basics.js
@@ -0,0 +1,5 @@
+// h1、`Reflect`基础
+
+// 1、Reflect是特殊的，她和`Object`不同，不是一个函数，不能被继承，没有`call`方法
+
+// 2、一些`Reflect`方法
---
+++ my storehouse/study/ES6 in es6katas.org/#60_Reflect_getPrototypeOf.js
@@ -0,0 +1,13 @@
+// h1、`Reflect.getPrototypeOf`返回一个原型
+
+// 1、和`Object.getPrototypeOf`效果一样
+
+// 2、当没有给定一个对象时抛出一个错误
+
+// 3、一个`new Set()`有原型
+
+// 4、对于一个class， 它是`Klass.prototype`
+
+// 5、对于旧形式的class，作用是一样的
+
+// 6、一个数组也有原型
