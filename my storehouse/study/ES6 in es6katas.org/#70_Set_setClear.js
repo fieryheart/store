87181012529e5fca1b23090b520e3c7d33f24b61
fieryheart <969216914@qq.com>
2017-02-10 13:40:52 +0800

    es6

---
+++ my storehouse/study/ES6 in es6katas.org/#70_Set_setClear.js
@@ -0,0 +1,9 @@
+// h1、`clear()`从一个Set对象中清除所有元素
+
+// 1、`set.size`变成0
+
+// 2、`set.entries()`迭代器将不包含任何值
+
+// 3、调用的`set.has()`返回false
+
+// 4、返回`undefined`
---
+++ my storehouse/study/ES6 in es6katas.org/#72_String_stringStartsWith.js
@@ -0,0 +1,7 @@
+// h1、`sre.startsWith(searchString)`决定str是否从给定的`startsWith`开始
+
+// 1、第一个参数是字符串查找的
+
+// 2、第二个参数是从哪里开始查找
+
+// 3、给这个函数变成增加功能性
