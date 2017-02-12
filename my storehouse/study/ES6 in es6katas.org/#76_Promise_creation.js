66083e6748b38e3a7daf6a957fa3e448ee6edf5e
fieryheart <969216914@qq.com>
2017-02-12 21:57:18 +0800

    es6

---
+++ my storehouse/study/ES6 in es6katas.org/#76_Promise_creation.js
@@ -0,0 +1,11 @@
+// h1、promise可以用多种方式创建
+
+// 1、当失败时
+
+// 2、大多数Promises使用构造函数创建
+
+// 3、扩展`Promise`
+
+// 4、`Promise.add()`当所有给予的promises resolve()时返回一个promise
+
+// 5、`Promise.race()`返回第一个稳定的promise
---
+++ my storehouse/study/ES6 in es6katas.org/#78_Promise_API.js
@@ -0,0 +1,12 @@
+// h1、`Promise` API 大致介绍
+
+// 1、`new Promise()`接受一个函数作为参数
+
+// 2、resolve一个promise
+
+// 3、一个rejected的promise
+
+// 4、`Promise.all()`
+
+// 5、`Promise.race()`
+
